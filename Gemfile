8d5f7a47f95f0db1a902aabb897c9a017516be34
Hector_Jimenez <hjmontemayor@gmail.com>
2020-11-01 16:54:19 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@
 source "https://rubygems.org"
 
 gem 'pry'
-gem 'sqlite3'
+sqlite3 -v '1.4.2'
+#gem 'sqlite3'
 gem 'activerecord', '~> 5.0', '>= 5.0.0.1'
--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,54 +60,59 @@ require 'active_support/inflector'
 
 class InteractiveRecord
 
-    def self.table_name
-      self.to_s.downcase.pluralize
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
     end
-  
-    def self.column_names
-      DB[:conn].results_as_hash = true
-  
-      sql = "pragma table_info('#{table_name}')"
-  
-      table_info = DB[:conn].execute(sql)
-      column_names = []
-      table_info.each do |row|
-        column_names << row["name"]
-      end
       column_names.compact
-    end
+  end
   
-    def initialize(options={})
-      options.each do |property, value|
-        self.send("#{property}=", value)
-      end
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
     end
+  end
   
-    def save
-      sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-      DB[:conn].execute(sql)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-    end
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
   
-    def table_name_for_insert
-      self.class.table_name
-    end
+  def table_name_for_insert
+    self.class.table_name
+  end
   
-    def values_for_insert
-      values = []
-      self.class.column_names.each do |col_name|
-        values << "'#{send(col_name)}'" unless send(col_name).nil?
-      end
-      values.join(", ")
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
+    values.join(", ")
+  end
   
-    def col_names_for_insert
-      self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-    end
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
   
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    sql = "SELECT grade, id, name FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(column)
+    sql = "SELECT grade, id, name FROM #{self.table_name} WHERE #{column.keys[0]} = ?"
+    DB[:conn].execute(sql, column.values[0])
   end
   
   end
